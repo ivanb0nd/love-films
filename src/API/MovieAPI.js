f4a974f7f443c08850d8f478356fa215860c1c0c
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-10-10 20:40:32 +0700

    Add API directory with MovieAPI, and create basic requests to API

---
+++ src/API/MovieAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_MOVIES_API_KEY;
+
+const onlineMovieAPI = axios.create({
+	baseURL: 'https://kinopoiskapiunofficial.tech/api/v2.2/',
+	headers: {
+		'X-API-KEY': API_KEY,
+		'Content-Type': 'application/json',
+	}
+})
+
+export default class movieAPI {
+	static async getMoviesByGenre(genre, page, moviesType, keyword,) {
+		const response = onlineMovieAPI.get(`films?genres=${genre}&page=${page}&type=${moviesType}&keyword=${keyword}`);
+		return response;
+	}
+
+	static async getPopularMovies(page) {
+		const response = onlineMovieAPI.get(`films/top?type=TOP_100_POPULAR_FILMS&page=${page}`)
+		return response;
+	}
+}
