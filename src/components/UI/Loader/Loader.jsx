324429963fe78c3caa5340e3b5e0854f6df1ff0b
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-10-17 23:01:28 +0700

    Add loader

---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Loader.module.css'
+
+const Loader = () => {
+	return (
+		<div class={classes.Roller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+	)
+}
+
+export default Loader
