a36ea534215abb47a962ff12cae0a1d3e76dc4c9
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-10-18 22:12:48 +0700

    Add request for fetching movies data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import CustomInput from '../../components/UI/Input/CustomInput'
+import Loader from '../../components/UI/Loader/Loader'
+import { useFetching } from '../../hooks/useFetching'
 import classes from './Home.module.css'
+import movieAPI from '../../API/MovieAPI'
+import Movies from '../../components/Movies/Movies'
 
 const Home = () => {
+	const [movies, setMovies] = useState([]);
+
+	const [fetchMovies, isMovieLoading, movieError] = useFetching(async () => {
+		const response = await movieAPI.getMoviesByGenre(1, 1, 'ALL')
+		setMovies(response.items)
+	})
+
+	useEffect(() => {
+		fetchMovies();
+	}, [])
+
+	console.log(movies)
 	return (
 		<div className={classes.home}>
 			<div className={classes.searchInput}>
@@ -13,7 +29,13 @@ const Home = () => {
 				<Sidebar />
 			</div>
 			<main className={classes.main}>
-				FILMS
+				{
+					movieError && <h1>Error {movieError}</h1>
+				}
+				{isMovieLoading
+					? <div className={classes.loaderWrapper}><Loader /></div>
+					: <Movies movies={movies} />
+				}
 			</main>
 		</div>
 	)
