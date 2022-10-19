e5e453a7d1373e5133efd8047f9d8b95e581aa52
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-10-19 19:24:08 +0700

    Create movie card

---
+++ src/components/UI/MovieItem/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './MovieCard.module.css'
+import StarIcon from '../../icons/StarIcon'
+import HeartIcon from '../../icons/HeartIcon'
+
+const MovieItem = ({ movie }) => {
+	return (
+		<Link className={classes.item} to={'/asdf'}>
+			<div className={classes.movie}>
+				<div className={classes.poster}>
+					<img src={movie.posterUrlPreview} alt={movie.type === 'TV_SERIES' ? `Постер сериала ${movie.nameRu}` : `Постер фильма ${movie.nameRu}`} />
+					<button className={classes.favoritesButton}><HeartIcon className={classes.favoritesButtonIcon} /></button>
+				</div>
+				<div className={classes.info}>
+					<h1 className={classes.title}>{movie.nameRu}</h1>
+					<div className={classes.rating}>
+						<StarIcon className={classes.ratingIcon} />
+						<span>{movie.ratingKinopoisk}</span>
+					</div>
+				</div>
+			</div>
+		</Link>
+	)
+}
+
+export default MovieItem
