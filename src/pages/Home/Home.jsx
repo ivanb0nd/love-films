ad84004d28f29c6dfd3a2b00c04eb7eb6d5697b2
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-10-28 23:34:18 +0700

    Add pagination and search film by keyword

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,58 +1,75 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import CustomInput from '../../components/UI/Input/CustomInput'
-import Loader from '../../components/UI/Loader/Loader'
-import { useFetching } from '../../hooks/useFetching'
-import classes from './Home.module.css'
+import React, { useEffect, useState } from 'react'
 import movieAPI from '../../API/MovieAPI'
 import Movies from '../../components/Movies/Movies'
-import { useInfiniteLoading } from '../../hooks/useInfiniteLoading'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import SearchButton from '../../components/UI/Buttons/SearchButton/SearchButton'
+import CustomInput from '../../components/UI/Input/CustomInput'
+import Loader from '../../components/UI/Loader/Loader'
+import Pagination from '../../components/UI/Pagination/Pagination'
+import { useFetching } from '../../hooks/useFetching'
+import classes from './Home.module.css'
 
 const Home = () => {
 	const [movies, setMovies] = useState([])
-	const [genre, setGenre] = useState(1)
-	const [page, setPage] = useState(1)
-	const [movieType, setMovieType] = useState('ALL')
 	const [totalPages, setTotalPages] = useState(1)
-	const infiniteLoadingContainer = useRef();
+	const [formingKeyword, setFormingKeyword] = useState('');
+	const [searchParams, setSearchParams] = useState({ page: 1, genre: 3, movieType: 'ALL', keyword: '' })
 
-	const [fetchMovies, isMovieLoading, movieError] = useFetching(async (genre, page, movieType) => {
-		const response = await movieAPI.getMoviesByGenre(genre, page, movieType)
+	const [fetchMovies, isMoviesLoading, moviesError] = useFetching(async (genre, page, movieType, keyword) => {
+		const response = await movieAPI.getMoviesByGenre(genre, page, movieType, keyword)
 		setTotalPages(response.totalPages)
-		setMovies([...movies, ...response.items])
+		setMovies(response.items)
 	})
 
-	function changeGenre(genre) {
-		setPage(1)
-		setGenre(genre)
+	const resetToDefaultSearchParams = () => {
+		setSearchParams({ page: 1, genre: 1, movieType: 'ALL', keyword: '' })
+	}
+
+	const changePage = (pageNumber) => {
+		setSearchParams({ ...searchParams, page: pageNumber })
+	}
+
+	const changeGenre = () => {
 	}
 
 	useEffect(() => {
-		fetchMovies(genre, page, movieType)
-	}, [genre, page, movieType])
+		fetchMovies(searchParams.genre, searchParams.page, searchParams.movieType, searchParams.keyword)
+	}, [searchParams.genre, searchParams.page, searchParams.movieType, searchParams.keyword, fetchMovies])
 
-	useInfiniteLoading(infiniteLoadingContainer, page < totalPages, isMovieLoading, () => setPage(page + 1))
-
-
-
+	function searchByKeyword(event) {
+		event.preventDefault()
+		setMovies([])
+		setTotalPages(1)
+		setSearchParams({ ...searchParams, keyword: formingKeyword, page: 1 })
+	}
 
 	return (
 		<div className={classes.home}>
-			<div className={classes.searchInput}>
-				<CustomInput placeholder="Поиск любого фильма или сериала" />
-			</div>
+			<form onSubmit={searchByKeyword} className={classes.searchForm}>
+				<CustomInput
+					value={formingKeyword}
+					onChange={(event) => setFormingKeyword(event.target.value)}
+					placeholder="Поиск любого фильма или сериала"
+				/>
+				<SearchButton />
+			</form>
 			<div className={classes.sidebar}>
-				<Sidebar genre={genre} changeGenre={changeGenre} />
+				<Sidebar genre={searchParams.genre} changeGenre={changeGenre} />
 			</div>
 			<main className={classes.main}>
 				{
-					movieError && <h1>Error {movieError}</h1>
+					moviesError && <h1>Error {moviesError}</h1>
 				}
-				<Movies setPage={setPage} page={page} movies={movies} />
-				<div ref={infiniteLoadingContainer} ></div>
-				{isMovieLoading &&
-					<div className={classes.loaderWrapper}><Loader /></div>
+				{isMoviesLoading
+					? <div className={classes.loaderWrapper}><Loader /></div>
+					: <Movies movies={movies} />
 				}
+				<Pagination
+					totalPage={totalPages}
+					page={searchParams.page}
+					changePage={changePage}
+					canLoad={isMoviesLoading}
+				/>
 			</main>
 		</div>
 	)
