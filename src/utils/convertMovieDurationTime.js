257dcd9563b8167bbf3dc1fc039c5466c7b18844
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-11 03:20:21 +0700

    update utility function

--- src/utils/convertMovieDurationTime.js
+++ src/utils/convertMovieDurationTime.js
@@ -1,11 +1,20 @@
 export const convertMovieDurationTime = (timeInMinutes) => {
 	let movieDurationInMinutes = timeInMinutes
 	let hours = 0
+	let minutes = 0
 
 	while (movieDurationInMinutes >= 60) {
 		movieDurationInMinutes -= 60
 		hours += + 1
 	}
 
-	return `${hours}:${movieDurationInMinutes}`
+	if (movieDurationInMinutes < 10) {
+		minutes = '0' + movieDurationInMinutes
+	} else {
+		minutes = movieDurationInMinutes
+	}
+
+
+
+	return `${hours}:${minutes}`
 }
