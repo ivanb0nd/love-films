0bbe5086941690408952f2f2ca1f55fa767b4848
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-13 23:12:40 +0700

    update popular movie card component

--- src/components/UI/PopularMovieCard/PopularMovieCard.jsx
+++ src/components/UI/PopularMovieCard/PopularMovieCard.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import classes from './PopularMovieCard.module.css'
-import StarIcon from '../../icons/StarIcon'
 import HeartIcon from '../../icons/HeartIcon'
+import StarIcon from '../../icons/StarIcon'
+import classes from './PopularMovieCard.module.css'
 
 const PopularMovieCard = ({ movie }) => {
+
 	return (
-		<Link className={classes.item} to={'/asdf'}>
-			<div className={classes.movie}>
+		<div className={classes.movie}>
+			<Link className={classes.item} to={`/movie/${movie.filmId}`}>
 				<div className={classes.poster}>
 					<img src={movie.posterUrlPreview} alt={movie.type === 'TV_SERIES' ? `Постер сериала ${movie.nameRu}` : `Постер фильма ${movie.nameRu}`} />
 					<button className={classes.favoritesButton}><HeartIcon className={classes.favoritesButtonIcon} /></button>
 				</div>
-				<div className={classes.info}>
-					<h1 className={classes.title}>{movie.nameRu}</h1>
-					<div className={classes.rating}>
-						<StarIcon className={classes.ratingIcon} />
-						<span>{movie.rating}</span>
-					</div>
+			</Link>
+			<div className={classes.info}>
+				<h1 className={classes.title}>{movie.nameRu}</h1>
+				<div className={classes.rating}>
+					<StarIcon className={classes.ratingIcon} />
+					<span>{movie.rating ? movie.rating : 'без рейтинга'}</span>
 				</div>
 			</div>
-		</Link>
+		</div>
+
 	)
 }
 
