203d5c46e722edb9dd59a479edb1940f6b50f910
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-13 22:59:43 +0700

    update movie footage component

--- src/components/UI/MovieFootage/MovieFootage.jsx
+++ src/components/UI/MovieFootage/MovieFootage.jsx
@@ -3,10 +3,17 @@ import FootageSlider from '../FootageSlider/FootageSlider';
 import classes from './MovieFootage.module.css';
 
 const MovieFootage = ({ footageData }) => {
+
 	return (
 		<div>
 			<h2 className={classes.title}>Кадры из фильма</h2>
-			<FootageSlider footageData={footageData} />
+			{footageData.length === 0
+				? <div className={classes.notReady}>
+					<p className={classes.text}>Кадры к фильму ещё не готовы <br /> Мы работаем над этим </p>
+					<span className={classes.emoji}>👨‍🔧</span>
+				</div>
+				: <FootageSlider footageData={footageData} />
+			}
 		</div>
 	)
 }
