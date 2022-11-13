8d74fab0bd4d31d29db208091e1149cb4504d57e
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-13 23:11:16 +0700

    Add new route

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,12 @@ import Home from "../pages/Home/Home";
 import Popular from "../pages/Popular/Popular";
 import About from "../pages/About/About";
 import Error from "../pages/Error/Error";
+import Movie from "../components/Movie/Movie";
 
 export const routes = [
 	{ path: '/home', element: <Home /> },
 	{ path: '/home/:genre', element: <Home /> },
+	{ path: '/movie/:movieId', element: <Movie /> },
 	{ path: '/favorites', element: <Favorites /> },
 	{ path: '/popular', element: <Popular /> },
 	{ path: '/about', element: <About /> },
