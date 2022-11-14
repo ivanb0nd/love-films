879f40a6d417bb0134c0de72f4066f02c804c71d
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-14 23:09:04 +0700

    Fix utility function

--- src/utils/convertMovieToObject.js
+++ src/utils/convertMovieToObject.js
@@ -1,11 +1,11 @@
 export const convertMovieToObject = (movie) => {
 	return {
-		id: movie.kinopoiskId ? movie.kinopoiskId : movie.imdbId,
+		id: movie.kinopoiskId ? movie.kinopoiskId : movie.imdbId ? movie.imdbId : movie.filmId ? movie.filmId : movie.id,
 		kinopoiskId: movie.kinopoiskId,
 		posterUrl: movie.posterUrl,
 		posterUrlPreview: movie.posterUrlPreview,
-		ratingImdb: movie.ratingImdb,
-		ratingKinopoisk: movie.ratingKinopoisk,
+		ratingImdb: movie.ratingImdb ? movie.ratingImdb : movie.rating,
+		ratingKinopoisk: movie.ratingKinopoisk ? movie.ratingKinopoisk : movie.rating,
 		nameRu: movie.nameRu,
 		nameOriginal: movie.nameOriginal,
 	}
