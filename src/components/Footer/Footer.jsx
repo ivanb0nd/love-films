8a611a39a7fc2344cecfb32f8624cb97cfd7a202
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-21 02:06:35 +0700

    Update footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
 		<footer className={classes.footer}>
 			<ul className={classes.footerList}>
 				<li className={classes.footerItem}>
-					Created with React <span className={classes.reactIcon}>⚛</span>
+					Создано на React <span className={classes.reactIcon}>⚛</span>
 				</li>
 				<li className={classes.footerItem}>
-					Data from <a className={classes.footerLink} href='https://kinopoiskapiunofficial.tech/' target='_blank' rel='noreferrer'>Kinopoisk Api Unofficial</a>
+					API проекта <a className={classes.footerLink} href='https://kinopoiskapiunofficial.tech/' target='_blank' rel='noreferrer'>Kinopoisk Api Unofficial</a>
 				</li>
 				<li className={classes.footerItem}>
-					Project on <a className={classes.footerLink} href='https://github.com/ivanb0nd/LoveFilms/' target='_blank' rel='noreferrer'>GitHub</a>
+					Проект на <a className={classes.footerLink} href='https://github.com/ivanb0nd/LoveFilms/' target='_blank' rel='noreferrer'>GitHub</a>
 				</li>
 				<li className={classes.footerItem}>
 					© by <a className={classes.footerLink} target='_blank' rel='noreferrer' href={'https://github.com/ivanb0nd'}>ivanb0nd</a>
