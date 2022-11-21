ba002ac91de5a39e72ebfb3219dacb190a453e63
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-22 00:03:44 +0700

    Add mobile menu in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import AppRouter from "./components/AppRouter/AppRouter";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import MobileMenu from "./components/UI/MobileMenu/MobileMenu";
 import { useTheme } from "./hooks/useTheme";
 import { FavoritesContext } from "./providers/FavoritesProvider/FavoritesProvider";
 import { ThemeContext } from "./providers/ThemeProvider/ThemeProvider";
@@ -33,6 +34,7 @@ function App() {
 					</div>
 				</div>
 				<Footer />
+				<MobileMenu />
 			</FavoritesContext.Provider>
 		</ThemeContext.Provider>
 	);
