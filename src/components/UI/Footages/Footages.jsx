5feaae00da70f542a68789648647bbe48eb9330c
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-11-21 23:58:24 +0700

    Update Footage component

---
+++ src/components/UI/Footages/Footages.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'swiper/css'
+import classes from './Footages.module.css'
+
+const FootageSlider = ({ footageData }) => {
+
+	return (
+		<div className={classes.footages}>
+			{footageData.map(footage => {
+				return <div key={footage.imageUrl} className={classes.footage}>
+					<picture>
+						<source srcSet={footage.imageUrl} media="(min-width: 900px)" />
+						<img src={footage.previewUrl} alt="" />
+					</picture>
+				</div>
+			})}
+		</div>
+	)
+}
+
+export default FootageSlider
